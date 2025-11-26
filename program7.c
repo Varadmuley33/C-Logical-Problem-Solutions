fb2e3bede20160de2b330cdd6ea320d338b67c7e
Varad Muley <varad.muley408@gmail.com>
2025-11-27 00:02:03 +0530

    Enhance documentation and comments in program7.c
    Added detailed comments and documentation for the program.

--- program7.c
+++ program7.c
@@ -1,25 +1,75 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program7.c
+//  Description :   Accepts two integers from user and returns their
+//                  addition using a user-defined function.
+//  Author :        Varad Nitin Muley
+//  Date :          16/10/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
-int AdditionTwoNumbers(int iNo1 , int iNo2)
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : AdditionTwoNumbers
+//  Description :   It performs addition of two integer values and
+//                  returns the result.
+//  Input :         Integer, Integer
+//  Output :        Integer
+//  Author :        Varad Nitin Muley
+//  Date :          16/10/2025
+//
+/////////////////////////////////////////////////////////////////
+
+int AdditionTwoNumbers(int iNo1, int iNo2)
 {
-    int sum = 0 ;
-    sum = iNo1 + iNo2;     // Business logic 
+    int sum = 0;                         // Variable to store sum
+    sum = iNo1 + iNo2;                   // Business logic
     return sum;
 }
 
-int main ()
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
 { 
-    int iValue1 = 0, iValue2 = 0 ;
-    int iRet = 0;
+    int iValue1 = 0, iValue2 = 0;        // Variables to store user input
+    int iRet = 0;                        // Variable to store result
    
     printf("Enter first number : ");
-    scanf("%d",&iValue1);
+    scanf("%d", &iValue1);
 
-    printf("Enter Second number : ");
-    scanf("%d",&iValue2);
+    printf("Enter second number : ");
+    scanf("%d", &iValue2);
 
-    iRet = AdditionTwoNumbers(iValue1,iValue2);
+    iRet = AdditionTwoNumbers(iValue1, iValue2);
 
-    printf("Addition is : %d\n" ,iRet);
+    printf("Addition is : %d\n", iRet);
     
     return 0;
-}
+}   // End of main
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : 5   10
+//  Output : Addition is : 15
+//
+//  Input : -10   20
+//  Output : Addition is : 10
+//
+//  Input : 0   0
+//  Output : Addition is : 0
+//
+/////////////////////////////////////////////////////////////////
