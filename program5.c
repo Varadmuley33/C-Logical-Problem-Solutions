b0f9098b507e75057c1cee4ca9865acb2a4f4ecb
Varad Muley <varad.muley408@gmail.com>
2025-11-27 00:00:12 +0530

    Refactor program5.c with comments and structure
    Added header comments, improved code structure, and clarified variable usage.

--- program5.c
+++ program5.c
@@ -1,19 +1,56 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program5.c
+//  Description :   Application which accepts two integers from user
+//                  and performs their addition.
+//  Author :        Varad Nitin Muley
+//  Date :          16/10/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
-int main ()
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
 { 
-    int i = 0, j = 0 ;
-    int ans = 0;
+    int i = 0, j = 0;                     // Variables to store user input
+    int ans = 0;                          // Variable to store result
 
     printf("Enter first number : ");
-    scanf("%d",&i);
+    scanf("%d", &i);
 
-    printf("Enter Second number : ");
-    scanf("%d",&j);
+    printf("Enter second number : ");
+    scanf("%d", &j);
     
-    ans = i + j;    // Business logic
+    ans = i + j;                          // Business logic
 
-    printf("Addition is : %d\n" ,ans);
+    printf("Addition is : %d\n", ans);   // Display result
     
     return 0;
-}
+}   // End of main
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : 10  20
+//  Output : Addition is : 30
+//
+//  Input : -5  15
+//  Output : Addition is : 10
+//
+//  Input : 0  0
+//  Output : Addition is : 0
+//
+/////////////////////////////////////////////////////////////////
