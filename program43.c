bd45f44d7c87d7b2ef2f1eff9f1ab7b82ea91ba8
Varad Muley <varad.muley408@gmail.com>
2025-12-01 23:27:42 +0530

    Refactor program43.c with documentation and formatting
    Added header comments and improved code structure.

--- program43.c
+++ program43.c
@@ -1,29 +1,63 @@
-//Input : 12     2                 Output : True
-//Input : 12     3                 Output : True
-//Input : 12     5                 Output : false
-//Input : 12     12                Output : false
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program43.c
+//  Description :   Checks whether the first number is completely divisible
+//                  by the second number using modulus operator.
+//  Author :        Varad Nitin Muley
+//  Date :          17/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
 
 #include<stdio.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
 int main()
 {
+    int iValue1 = 0, iValue2 = 0;
 
-    int iValue1 = 0 , iValue2 = 0;
+    printf("Enter first Number : ");
+    scanf("%d", &iValue1);
 
-    printf ("Enter first Number : ");
-    scanf("%d",&iValue1);
+    printf("Enter second Number : ");
+    scanf("%d", &iValue2);
 
-    printf ("Enter Second Number : ");
-    scanf("%d",&iValue2);
-
-    if((iValue1 % iValue2) == 0)
+    if((iValue1 % iValue2) == 0)                // Check divisibility
     {
-        printf("It is completely divisible");
+        printf("It is completely divisible\n");
     }
     else
     {
-        printf("It is not completely divisible");
+        printf("It is not completely divisible\n");
     }
-    return 0;
 
-}
+    return 0;
+}   // End of main
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : 12   2
+//  Output : It is completely divisible
+//
+//  Input : 12   3
+//  Output : It is completely divisible
+//
+//  Input : 12   5
+//  Output : It is not completely divisible
+//
+//  Input : 12   12
+//  Output : It is completely divisible
+//
+/////////////////////////////////////////////////////////////////
