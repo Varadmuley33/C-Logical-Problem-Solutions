1f572ace035091570ed169de0480797d30c7a489
Varad Muley <varad.muley408@gmail.com>
2025-12-02 00:45:32 +0530

    Add files via upload

---
+++ program56.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+
+int checkPrime(int iNo)
+{
+   int iCnt = 0;
+  
+   if(iNo < 0)
+   {
+     iNo = -iNo;
+   }
+   
+   for(iCnt = 2 ; iCnt <= (iNo/2) ; iCnt++)
+   {
+     if((iNo % iCnt ) == 0)
+     {
+          break;                        // Optimization
+     }
+   }
+   if(iCnt > (iNo/2))                  // No factors            magchya code peksha ithe changes kele ajun vegla approach
+   {
+       return true ;
+   }
+   else                                // Atleast one factor
+   { 
+       return false;
+   }
+}
+int main ()
+{
+     int iValue = 0;
+     bool bRet = 0 ;
+
+     printf("Enter the number : \n");
+     scanf("%d",&iValue);
+
+     bRet = checkPrime(iValue);
+
+    if(bRet == true )
+      {
+        printf("%d is a prime number \n",iValue);
+      } 
+    else
+      {
+        printf("%d is a not prime number",iValue);
+      }
+
+    return 0;
+
+}
+
+/*
+    Time Complexity for Prime : N/2
+    Time Complexity for Non Prime : either 1 or 2 
+*/
---
+++ program57.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+
+int checkPrime(int iNo)
+{
+   int iCnt = 0;
+
+   bool bFlag = true;
+  
+   if(iNo < 0)
+   {
+     iNo = -iNo;
+   }
+   
+   for(iCnt = 2 ; iCnt <= (iNo/2) ; iCnt++)
+   {
+     if((iNo % iCnt ) == 0)
+     {
+          bFlag = false;
+          break;                        // Optimization
+     }
+   }
+   return bFlag;
+}
+int main ()
+{
+     int iValue = 0;
+     bool bRet = 0 ;
+
+     printf("Enter the number : \n");
+     scanf("%d",&iValue);
+
+     bRet = checkPrime(iValue);
+
+    if(bRet == true )
+      {
+        printf("%d is a prime number \n",iValue);
+      } 
+    else
+      {
+        printf("%d is a not prime number",iValue);
+      }
+
+    return 0;
+
+}
+
+/*
+    Time Complexity for Prime : N/2
+    Time Complexity for Non Prime : either 1 or 2 
+*/
