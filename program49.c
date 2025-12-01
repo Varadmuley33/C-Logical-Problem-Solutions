ab33576c6b3738c3e08e513f7dfb61c0edb11b52
Varad Muley <varad.muley408@gmail.com>
2025-12-01 23:42:34 +0530

    Add documentation and comments to program49.c
    Refactored program to include detailed comments and documentation.

--- program49.c
+++ program49.c
@@ -1,37 +1,79 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program49.c
+//  Description :   Accepts a number from the user and displays all its
+//                  non-factors using a loop. Handles negative input.
+//  Author :        Varad Nitin Muley
+//  Date :          18/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : DisplayNonFactors
+//  Description :   Prints all non-factors of iNo
+//  Input :         Integer
+//  Output :        void
+//
+/////////////////////////////////////////////////////////////////
 
 void DisplayNonFactors(int iNo)
 {
-     int iCnt = 0 ;
+    int iCnt = 0;
 
+    // Updator: Convert negative number to positive
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
 
-     if(iNo < 0 )
-     {
-          iNo = -iNo;
-     }
-
-     for(iCnt = 1 ; iCnt < iNo ; iCnt ++)
-     {
-          if((iNo % iCnt) != 0)
-          {
-               printf("%d\n",iCnt);
-          }
-     }
+    // Display all non-factors from 1 to (iNo - 1)
+    for(iCnt = 1; iCnt < iNo; iCnt++)
+    {
+        if((iNo % iCnt) != 0)                // Non-factor condition
+        {
+            printf("%d\n", iCnt);
+        }
+    }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
 
 int main()
 {
-    int iValue = 0 ;
+    int iValue = 0;
 
-    printf ("Enter  Number : ");
-    scanf("%d",&iValue);
+    printf("Enter Number : ");
+    scanf("%d", &iValue);
 
-    DisplayNonFactors(iValue);
+    DisplayNonFactors(iValue);               // Function call
 
     return 0;
+}   // End of main
 
-}
-
-
-// Time Comlexity : O(N)
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : 10
+//  Output : 3  4  6  7  8  9
+//
+//  Input : 8
+//  Output : 3  5  6  7
+//
+//  Input : -12
+//  Output : 5  7  8  10  11   (negative handled by updator)
+//
+/////////////////////////////////////////////////////////////////
