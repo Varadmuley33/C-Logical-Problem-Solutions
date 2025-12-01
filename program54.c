ce35498f769e335e0522eb33bc5d5e05fedf1bfb
Varad Muley <varad.muley408@gmail.com>
2025-12-02 00:45:00 +0530

    Add files via upload

---
+++ program54.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+
+int checkPrime(int iNo)
+{
+   int iCnt = 0;
+   int iFrequency = 0;
+   
+   if(iNo < 0)
+   {
+     iNo = -iNo;
+   }
+   
+   for(iCnt = 2 ; iCnt <= (iNo/2) ; iCnt++)
+   {
+     if((iNo % iCnt ) == 0)
+     {
+          iFrequency++;
+     }
+   }
+   if(iFrequency == 0)
+   {
+       return true ;
+   }
+   else 
+   {
+       return false;
+   }
+}
+int main ()
+{
+     int iValue = 0;
+     bool bRet = 0 ;
+
+     printf("Enter the number : \n");
+     scanf("%d",&iValue);
+
+     bRet = checkPrime(iValue);
+
+    if(bRet == true )
+      {
+        printf("%d is a prime number \n",iValue);
+      } 
+    else
+      {
+        printf("%d is a not prime number",iValue);
+      }
+
+    return 0;
+
+}
