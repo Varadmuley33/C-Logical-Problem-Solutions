adc94c818f63f664c8865f32621a27711e95d9a1
Varad Muley <varad.muley408@gmail.com>
2025-12-01 23:41:44 +0530

    Update program47.c

--- program47.c
+++ program47.c
@@ -1,28 +1,72 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program47.c
+//  Description :   Displays all factors of a user-entered number using
+//                  an efficient loop-based approach.
+//  Author :        Varad Nitin Muley
+//  Date :          18/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : DisplayFactors
+//  Description :   Prints all factors of iNo using a loop
+//  Input :         Integer
+//  Output :        void
+//
+/////////////////////////////////////////////////////////////////
 
 void DisplayFactors(int iNo)
 {
-     int iCnt = 0 ;
+    int iCnt = 0;                           // Loop counter
 
-     for(iCnt = 1 ; iCnt < iNo ; iCnt ++)
-     {
-          if((iNo % iCnt) == 0)
-          {
-               printf("%d\n",iCnt);
-          }
-     }
+    for(iCnt = 1; iCnt < iNo; iCnt++)       // Check from 1 to (iNo - 1)
+    {
+        if((iNo % iCnt) == 0)               // Factor condition
+        {
+            printf("%d\n", iCnt);           // Print factor
+        }
+    }
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
 
 int main()
 {
-    int iValue = 0 ;
+    int iValue = 0;
 
-    printf ("Enter  Number : ");
-    scanf("%d",&iValue);
+    printf("Enter Number : ");
+    scanf("%d", &iValue);
 
-    DisplayFactors(iValue);
+    DisplayFactors(iValue);                 // Function call
 
     return 0;
+}   // End of main
 
-}
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : 6
+//  Output : 1  2  3
+//
+//  Input : 12
+//  Output : 1  2  3  4  6
+//
+//  Input : 10
+//  Output : 1  2  5
+//
+/////////////////////////////////////////////////////////////////
