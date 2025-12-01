a381a556a84a41af9aeeb84a0f5c218ce32faada
Varad Muley <varad.muley408@gmail.com>
2025-12-01 23:28:23 +0530

    Refactor program to check divisibility with comments
    The program checks if the first number is divisible by the second using a user-defined function. Added comments and improved formatting for clarity.

--- program44.c
+++ program44.c
@@ -1,15 +1,34 @@
-//Input : 12     2                 Output : True
-//Input : 12     3                 Output : True
-//Input : 12     5                 Output : false
-//Input : 12     12                Output : false
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program44.c
+//  Description :   Checks whether the first number is completely divisible
+//                  by the second number using a user-defined function.
+//  Author :        Varad Nitin Muley
+//  Date :          18/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
 
 #include<stdio.h>
 #include<stdbool.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : CheckDivisible
+//  Description :   Returns true if iNo1 is completely divisible by iNo2
+//  Input :         Integer, Integer
+//  Output :        Boolean
+//
+/////////////////////////////////////////////////////////////////
 
-bool CheckDivisible(int iNo1 , int iNo2 )
+bool CheckDivisible(int iNo1, int iNo2)
 {
-    if((iNo1 % iNo2 ) == 0)
+    if((iNo1 % iNo2) == 0)                    // Divisibility check
     {
         return true;
     }
@@ -19,28 +38,51 @@ bool CheckDivisible(int iNo1 , int iNo2 )
     }
 }
 
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
 int main()
 {
-
-    int iValue1 = 0 , iValue2 = 0;
+    int iValue1 = 0, iValue2 = 0;
     bool bRet = false;
 
-    printf ("Enter first Number : ");
-    scanf("%d",&iValue1);
+    printf("Enter first Number : ");
+    scanf("%d", &iValue1);
 
-    printf ("Enter Second Number : ");
-    scanf("%d",&iValue2);
+    printf("Enter second Number : ");
+    scanf("%d", &iValue2);
 
-    bRet = CheckDivisible(iValue1 , iValue2);
+    bRet = CheckDivisible(iValue1, iValue2);    // Function call
 
     if(bRet == true)
     {
-        printf("It is completely divisible");
+        printf("It is completely divisible\n");
     }
     else
     {
-        printf("It is not completely divisible");
+        printf("It is not completely divisible\n");
     }
-    return 0;
 
-}
+    return 0;
+}   // End of main
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input  : 12   2
+//  Output : It is completely divisible
+//
+//  Input  : 12   3
+//  Output : It is completely divisible
+//
+//  Input  : 12   5
+//  Output : It is not completely divisible
+//
+//  Input  : 12   12
+//  Output : It is completely divisible
+//
+/////////////////////////////////////////////////////////////////
