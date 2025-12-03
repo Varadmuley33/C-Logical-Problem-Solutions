12795ecea3e9cd2984943fb935878c245b88d503
Varad Muley <varad.muley408@gmail.com>
2025-12-03 23:44:47 +0530

    Update program67.c

--- program67.c
+++ program67.c
@@ -1,35 +1,76 @@
-// Iteration 
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program67.c
+//  Description :   Displays each digit of the given number using iterative
+//                  division and modulus operations.
+//  Author :        Varad Nitin Muley
+//  Date :          20/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : DisplayDigits
+//  Description :   Extracts and displays digits of the number
+//                  one by one using iteration.
+//  Input :         Integer
+//  Output :        None
+//
+/////////////////////////////////////////////////////////////////
 
 void DisplayDigits(int iNo)
 {
-     int iDigit = 0;
+    int iDigit = 0;             // To store extracted digit
 
-     printf("-----------------------------------------------------\n");
+    printf("-----------------------------------------------------\n");
+    printf("Original value of iNo is : %d\n", iNo);
 
-     printf("Original value of iNo is : %d\n",iNo );
+    // Iterate until all digits are processed
+    while(iNo != 0)
+    {
+        printf("-----------------------------------------------------\n");
 
-     while(iNo != 0)
-     {
-          printf("-----------------------------------------------------\n");
-          iDigit = iNo % 10;
-          printf(" iDigit is : %d\n",iDigit);
-          iNo = iNo / 10;
-          printf(" iNo is : %d\n",iNo);
-     }
+        iDigit = iNo % 10;      // Extract last digit
+        printf("iDigit is : %d\n", iDigit);
 
-     printf("-----------------------------------------------------\n");
+        iNo = iNo / 10;         // Remove last digit
+        printf("iNo is : %d\n", iNo);
+    }
+
+    printf("-----------------------------------------------------\n");
 }
 
-int main ()
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
 {
-     int iValue = 0;
-     
-     printf("Enter the number : ");
-     scanf("%d",&iValue);
+    int iValue = 0;
 
-     DisplayDigits(iValue);
+    printf("Enter the number : ");
+    scanf("%d", &iValue);
 
-     return 0;
+    DisplayDigits(iValue);      // Function call
 
-}
+    return 0;
+}   // End of main
+
+/////////////////////////////////////////////////////////////////
+//
+//  Note : This program demonstrates digit extraction using
+//         iteration. Each step prints intermediate results.
+//
+//  Time Complexity : O(N)   (N = number of digits)
+//
+/////////////////////////////////////////////////////////////////
