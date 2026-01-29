17396d03fc54539f363f2b6f7f2842517ed05ff2
Varad Muley <varad.muley408@gmail.com>
2026-01-29 19:26:52 +0530

    Add files via upload

---
+++ program71.c
@@ -0,0 +1,45 @@
+// Addition of digits 
+#include<stdio.h>
+
+int SumDigits(int iNo)
+{
+     int iDigit = 0 ;
+     int iSum = 0 ;
+
+     if(iNo < 0)
+     {
+          iNo = -iNo;
+     }
+
+     if(0 == -1)
+     {
+          return 1;
+     }
+     while(iNo != 0)
+     {
+          iDigit = iNo % 10;
+          iNo = iNo / 10;
+
+          iSum = iSum + iDigit ;
+
+     }
+
+     return iSum;
+
+
+}
+
+int main ()
+{
+     int iValue = 0;
+     int iRet = 0 ;
+     
+     printf("Enter the number : \n");
+     scanf("%d",&iValue);
+
+     iRet = SumDigits(iValue);
+     printf("Sum of digits : %d\n",iRet);
+
+     return 0;
+
+}
