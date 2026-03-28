59809416ad3d529abcc7d8a67439686bb672d078
Varad Muley <varad.muley408@gmail.com>
2026-03-28 23:46:01 +0530

    Update Program271.c

---
+++ Program271.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+float Summation (float Arr[] , int iSize)
+{
+    float iSum;
+    int iCnt = 0;
+
+    for(iCnt = 0 ; iCnt < iSize ; iCnt++)
+    {
+        iSum = iSum + Arr[iCnt];
+    }
+
+    return iSum;
+}
+
+int main ()
+{
+    float Brr[] = {10.1f ,20.2f , 30.3f , 40.4f , 50.5f };
+    float fRet = 0.0f;
+
+    fRet = Summation(Brr , 5);
+
+    printf("Summation is :%f\n",fRet);
+
+    return 0;
+}
