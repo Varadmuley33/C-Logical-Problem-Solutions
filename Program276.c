2e953db243bb86c8dd99f597d041839e92aebb6f
Varad Muley <varad.muley408@gmail.com>
2026-03-29 22:10:56 +0530

    Update Program276.c

---
+++ Program276.c
@@ -0,0 +1,17 @@
+// POP Approach
+
+#include<stdio.h>
+
+int Addition(int No1 , int No2)
+{
+    int Ans = 0 ;
+    Ans = No1 + No2;
+    return Ans;
+}
+
+int main()
+{
+    printf("%d\n", Addition(10,11));
+
+    return 0;
+}
