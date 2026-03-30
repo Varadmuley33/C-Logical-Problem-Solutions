a9ad3bd3e5201f6fe7472a365f43b5ea6ca8f9d7
Varad Muley <varad.muley408@gmail.com>
2026-03-30 23:23:02 +0530

    Update Program285.c

---
+++ Program285.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void Display()
+{
+    int iCnt = 0;
+    printf("Jay Ganesh ...%d\n", iCnt);
+    iCnt++;
+    Display();
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
