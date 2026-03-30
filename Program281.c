78041c7db778808d853f4922734366e0410579ca
Varad Muley <varad.muley408@gmail.com>
2026-03-30 23:16:58 +0530

    Update Program281.c

---
+++ Program281.c
@@ -0,0 +1,20 @@
+// OOP Apporach (Generic)
+#include<iostream>
+using namespace std ;
+
+
+
+void Display()
+{
+    cout<<"Jay Ganesh...\n";
+    cout<<"Jay Ganesh...\n";
+    cout<<"Jay Ganesh...\n";
+    cout<<"Jay Ganesh...\n";
+}
+
+int main ()
+{
+    Display();
+    return 0;
+} 
+
