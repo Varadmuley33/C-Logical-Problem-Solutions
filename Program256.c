0635c6834f72e4406b2cfcdb0be7eedcf0884588
Varad Muley <varad.muley408@gmail.com>
2026-03-31 23:46:41 +0530

    Update Program256.c

---
+++ Program256.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+struct StringX
+{
+};
+
+char* StrToggleX(char str[])
+{
+    int iCnt = 0;
+
+    static char Arr[100];
+
+    strcpy(Arr, str);
+
+    for(iCnt = 0; iCnt < strlen(Arr); iCnt++)
+    {
+        if((Arr[iCnt] >= 'A') && (Arr[iCnt] <= 'Z'))
+        {
+            Arr[iCnt] = Arr[iCnt] + 32;
+        }
+        else if((Arr[iCnt] >= 'a') && (Arr[iCnt] <= 'z'))
+        {
+            Arr[iCnt] = Arr[iCnt] - 32;
+        }
+    }
+
+    return Arr;
+}
+
+int main()
+{
+    char *sRet = NULL;
+    char sobj[100];
+
+    printf("Enter string : \n");
+    fgets(sobj, sizeof(sobj), stdin);
+
+    struct StringX strobj;
+
+    sRet = StrToggleX(sobj);
+
+    printf("Updated string  is : %s\n", sRet);
+
+    return 0;
+}
