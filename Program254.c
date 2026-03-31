93c47f5985a87c3820b26f56dcadec9f08aa7723
Varad Muley <varad.muley408@gmail.com>
2026-03-31 23:45:38 +0530

    Add Program254.c to update string with underscore

---
+++ Program254.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+struct StringX
+{
+};
+
+char* Update(char str[])
+{
+    static char Arr[100];
+
+    strcpy(Arr, str);
+
+    Arr[0] = '_';
+
+    return Arr;
+}
+
+int main()
+{
+    char sobj[100];
+
+    printf("Enter string : \n");
+    fgets(sobj, sizeof(sobj), stdin);
+
+    struct StringX strobj;
+
+    strcpy(sobj, Update(sobj));
+
+    printf("Updated string  is : %s\n", sobj);
+
+    return 0;
+}
