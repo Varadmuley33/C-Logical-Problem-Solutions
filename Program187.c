3c3520ceeba3a042b685fe2e1af182ff0f284a72
Varad Muley <varad.muley408@gmail.com>
2026-04-04 23:49:12 +0530

    Update Program187.c

---
+++ Program187.c
@@ -0,0 +1,113 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     program187.c
+//  Description :   
+//  Author :        Varad Nitin Muley
+//  Date :          18/11/2025
+//  Input :         6
+//  Output :        
+//       
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+    iRow = 4    iCol = 4
+
+       $ * * * 
+       * $ * *
+       * * $ *
+       * * * $
+       
+       Diagonal pattern 
+*/
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function name : Display
+//  Description   : Displays diagonal pattern with square filter
+//  Input         : Integer, Integer
+//  Output        : void
+//  Author        : Varad Nitin Muley
+//  Date          : 18/11/2025
+//
+/////////////////////////////////////////////////////////////////
+
+void Display(int iRow,int iCol)
+{
+    int i = 0 ,j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid Input\n");
+        printf("Row number and column number should be same\n");
+        return;
+    }
+
+    for (i = 1 ; i <= iRow ; i++)
+    {
+        for (j = 1 ; j <= iCol ; j++)
+        {
+            if((i == j))
+            {
+                printf("$\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\n"); 
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0 ,iValue2 = 0 ;
+
+    printf("Enter the number of rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of columns : \n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases :
+//
+//  Input  : 4 4
+//  Output : 
+//           $ * * *
+//           * $ * *
+//           * * $ *
+//           * * * $
+//
+//  Input  : 3 5
+//  Output : 
+//           Invalid Input
+//           Row number and column number should be same
+//
+//  Input  : 2 2
+//  Output : 
+//           $ *
+//           * $
+//
+/////////////////////////////////////////////////////////////////
