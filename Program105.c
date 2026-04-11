b5c6280a13432e95ca6c76214213d7de14f9c077
Varad Muley <varad.muley408@gmail.com>
2026-04-11 23:36:39 +0530

    Update Program105.c

---
+++ Program105.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void Accept(int Arr[], int iSize)
+{
+    int i = 0;
+
+    printf("Enter the elements of Array : \n");
+
+    for(i = 0; i < iSize; i++)
+    {
+        scanf("%d",&Arr[i]);
+    }
+}
+
+void Display(int Arr[], int iSize)
+{
+    int i = 0;
+
+    printf("elements of the array are \n");
+
+    for(i = 0; i < iSize; i++)
+    {
+        printf("%d\n",Arr[i]);
+    }
+}
+
+int main()
+{
+    int *Arr1 = NULL;
+    int iSize1 = 5;
+
+    printf("Inside constructor\n");
+    Arr1 = (int *)malloc(iSize1 * sizeof(int));
+
+    printf("%d\n", iSize1);
+
+    iSize1 = 11;
+    Arr1 = NULL;
+
+    int *Arr2 = NULL;
+    int iSize2 = 7;
+
+    printf("Inside constructor\n");
+    Arr2 = (int *)malloc(iSize2 * sizeof(int));
+
+    printf("%d\n", iSize2);
+
+    return 0;
+}
