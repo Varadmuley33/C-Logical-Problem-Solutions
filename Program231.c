69e58b4baf22ba669372327bf65f4c352f3f32e6
Varad Muley <varad.muley408@gmail.com>
2026-06-10 22:09:58 +0530

    Refactor Program231.c with comments and structure
    Added comments and structured the code for clarity.

--- Program231.c
+++ Program231.c
@@ -1,5 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     CountCapitalSmall.c
+//  Description :   Counts the number of capital and small characters
+//                  present in the given string.
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : CountAll
+//  Description :   Counts capital and small alphabetic characters
+//                  from the given string.
+//  Input :         Character Array
+//  Output :        Void
+//  Author :        Varad Nitin Muley
+//
+/////////////////////////////////////////////////////////////////
 
 void CountAll(char str[])
 {
@@ -16,19 +41,22 @@ void CountAll(char str[])
         {
             iCountSmall++;
         }
-        str++;
 
+        str++;
     }
 
     printf("Count of Capital characters is : %d\n" ,iCountCapital);
     printf("Count of Small characters is : %d\n" ,iCountSmall);
-
-
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
 
 int main ()
 {
-
     char Arr[50] = {'\0'};
 
     printf("Enter string : \n");
@@ -38,3 +66,21 @@ int main ()
 
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : Abc
+//  Output : Count of Capital characters is : 1
+//           Count of Small characters is : 2
+//
+//  Input : HELLO
+//  Output : Count of Capital characters is : 5
+//           Count of Small characters is : 0
+//
+//  Input : abcDEF
+//  Output : Count of Capital characters is : 3
+//           Count of Small characters is : 3
+//
+/////////////////////////////////////////////////////////////////
