3a3a90e3328997dad13133b39216d39cc2a366f3
Varad Muley <varad.muley408@gmail.com>
2026-06-14 23:27:20 +0530

    Add comments for clarity and documentation
    This update adds comments to explain the purpose of the program and its functions, enhancing code readability.

--- Program258.c
+++ Program258.c
@@ -11,7 +11,89 @@ void strcatX(char *src , char *dest)
     {
         *dest = *src;
         src++;
+        dest++;////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program258.c
+//  Description :   Accepts source and destination strings from the user,
+//                  appends the source string to the end of the
+//                  destination string, and displays the updated string.
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////////////////
+//
+//  Appends the source string at the end of the
+//  destination string
+//
+/////////////////////////////////////////////////////////////////
+
+void strcatX(char *src , char *dest)
+{
+    while(*dest != '\0')
+    {
         dest++;
+    }
+
+    while(*src != '\0')
+    {
+        *dest = *src;
+        src++;
+        dest++;
+    }
+
+    *dest = '\0';    // *dest = *src; works same like the written condition
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    char Brr[50] = {'\0'};
+
+    printf("Enter Source string : \n");
+    scanf("%[^'\n']s",Arr);
+
+    printf("Enter destination string : \n");
+    scanf(" %[^'\n']s",Brr);
+
+    strcatX(Arr,Brr);
+
+    printf("Udpdated String is : %s\n",Brr);
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : Hello
+//          World
+//  Output : WorldHello
+//
+//  Input : Infosystems
+//          Marvellous
+//  Output : MarvellousInfosystems
+//
+//  Input : City
+//          Pune
+//  Output : PuneCity
+//
+/////////////////////////////////////////////////////////////////
     }
 
     *dest = '\0';    // *dest = *src; works same like the written condition 
