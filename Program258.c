76274e50b7e11d469081b5dff87c450807ae5d56
Varad Muley <varad.muley408@gmail.com>
2026-06-15 23:11:21 +0530

    Refactor Program258.c to remove duplicate code

--- Program258.c
+++ Program258.c
@@ -95,24 +95,3 @@ int main()
 //  Output : PuneCity
 //
 /////////////////////////////////////////////////////////////////
-    }
-
-    *dest = '\0';    // *dest = *src; works same like the written condition 
-}
-int main() 
-{
-    char Arr[50] = {'\0'};
-    char Brr[50] = {'\0'};
-
-    printf("Enter Source string : \n");
-    scanf("%[^'\n']s",Arr);
-
-    printf("Enter destination string : \n");
-    scanf(" %[^'\n']s",Brr);
-
-    strcatX(Arr,Brr);
-
-    printf("Udpdated String is : %s\n",Brr);
-
-    return 0;
-}
