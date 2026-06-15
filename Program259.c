eb3064c3a5b4e0f3401cac6151d1e103c9e0bc21
Varad Muley <varad.muley408@gmail.com>
2026-06-15 23:10:41 +0530

    Refactor Program259.c to remove duplicate code

--- Program259.c
+++ Program259.c
@@ -93,14 +93,3 @@ int main()
     printf("\n");
 
 }
-int main() 
-{
-    char Arr[50] = {'\0'};
-
-    printf("Enter  string : \n");
-    scanf("%[^'\n']s",Arr);
-
-    ReverseDisplay(Arr);
-
-    return 0;
-}
