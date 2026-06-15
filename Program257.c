e3c347539e4109fcea20c70024397be3bb250d98
Varad Muley <varad.muley408@gmail.com>
2026-06-15 23:12:05 +0530

    Refactor Program257.c for clarity and structure

--- Program257.c
+++ Program257.c
@@ -80,21 +80,3 @@ int main()
 //  Output : Pune
 //
 /////////////////////////////////////////////////////////////////
-    }
-
-    *dest = '\0';
-}
-int main() 
-{
-    char Arr[50] = {'\0'};
-    char Brr[50] = {'\0'};
-
-    printf("Enter string : \n");
-    scanf("%[^'\n']s",Arr);
-
-    strcpyX(Arr,Brr);
-
-    printf("Copied String is : %s\n",Brr);
-
-    return 0;
-}
