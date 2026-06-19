b3d463ccb1cf8c9f846a48c41a0005819879d7e6
Varad Muley <varad.muley408@gmail.com>
2026-06-19 23:35:11 +0530

    Refactor Program222.c with documentation and comments
    Added comments and documentation for clarity.

--- Program222.c
+++ Program222.c
@@ -1,36 +1,84 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program222.c
+//  Description :   Counts the number of vowels (both uppercase and
+//                  lowercase) present in the given string.
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
-// a e i o u 
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : CountVowels
+//  Description :   Counts the number of vowels (both uppercase
+//                  and lowercase) present in the given string.
+//  Input :         Character Array
+//  Output :        Integer
+//  Author :        Varad Nitin Muley
+//
+/////////////////////////////////////////////////////////////////
+
 int CountVowels(char str[])
 {
-    int iCount = 0 ;
+    int iCount = 0;
 
     while(*str != '\0')
     {
-        if((*str == 'e') || (*str == 'a') || (*str == 'i') || (*str == 'o') || (*str == 'u')|| 
-            (*str == 'A') || (*str == 'E') || (*str == 'I') || (*str == 'O')|| (*str == 'U'))
+        if((*str == 'e') || (*str == 'a') || (*str == 'i') || (*str == 'o') || (*str == 'u') ||
+           (*str == 'A') || (*str == 'E') || (*str == 'I') || (*str == 'O') || (*str == 'U'))
         {
-          iCount++;
+            iCount++;
         }
+
         str++;
     }
 
     return iCount;
-
 }
 
-int main ()
-{
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
 
+int main()
+{
     char Arr[50] = {'\0'};
-    int iRet = 0 ;
+    int iRet = 0;
 
     printf("Enter string : \n");
-    scanf("%[^'\n]s",Arr);
+    scanf("%[^'\n]s", Arr);
 
     iRet = CountVowels(Arr);
 
-    printf("Count of vowels are  : %d\n",iRet);
+    printf("Count of vowels are : %d\n", iRet);
 
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : Hello
+//  Output : Count of vowels are : 2
+//
+//  Input : HELLO
+//  Output : Count of vowels are : 2
+//
+//  Input : Marvellous Infosystems
+//  Output : Count of vowels are : 7
+//
+//  Input : XYZ
+//  Output : Count of vowels are : 0
+//
+/////////////////////////////////////////////////////////////////
