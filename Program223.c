feeba8719ff0b40eb350d846fd6ae9873f829625
Varad Muley <varad.muley408@gmail.com>
2026-06-19 23:35:36 +0530

    Enhance comments and documentation in Program223.c
    Added header comments for file description, function details, and test cases.

--- Program223.c
+++ Program223.c
@@ -1,35 +1,83 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program223.c
+//  Description :   Counts the number of occurrences of a specific
+//                  character in the given string.
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : CountOccurance
+//  Description :   Counts the number of occurrences of the
+//                  specified character in the given string.
+//  Input :         Character Array, Character
+//  Output :        Integer
+//  Author :        Varad Nitin Muley
+//
+/////////////////////////////////////////////////////////////////
 
-int CountOccurance(char str[] , char ch)
+int CountOccurance(char str[], char ch)
 {
-    int iCount = 0 ;
+    int iCount = 0;
 
     while(*str != '\0')
     {
         if(*str == ch)
         {
-          iCount++;
+            iCount++;
         }
+
         str++;
     }
 
     return iCount;
-
 }
 
-int main ()
-{
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
 
+int main()
+{
     char Arr[50] = {'\0'};
-    int iRet = 0 ;
+    int iRet = 0;
 
     printf("Enter string : \n");
-    scanf("%[^'\n]s",Arr);
+    scanf("%[^'\n]s", Arr);
 
-    iRet = CountOccurance(Arr ,'d');
+    iRet = CountOccurance(Arr, 'd');
 
-    printf("Number of occurance are  : %d\n",iRet);
+    printf("Number of occurance are : %d\n", iRet);
 
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : India
+//  Output : Number of occurance are : 1
+//
+//  Input : Marvellous
+//  Output : Number of occurance are : 0
+//
+//  Input : adddd
+//  Output : Number of occurance are : 4
+//
+//  Input : dddddd
+//  Output : Number of occurance are : 6
+//
+/////////////////////////////////////////////////////////////////
