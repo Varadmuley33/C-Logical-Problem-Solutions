e303c424072b9dac31f64b7262a60c0df9256bee
Varad Muley <varad.muley408@gmail.com>
2026-06-19 23:35:53 +0530

    Refactor Program224.c with comments and structure
    Added header comments and improved code structure.

--- Program224.c
+++ Program224.c
@@ -1,39 +1,91 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program224.c
+//  Description :   Counts the number of occurrences of a user-specified
+//                  character in the given string.
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : CountOccurance
+//  Description :   Counts the number of occurrences of the
+//                  specified character in the given string.
+//  Input :         Character Array, Character
+//  Output :        Integer
+//  Author :        Varad Nitin Muley
+//
+/////////////////////////////////////////////////////////////////
 
-int CountOccurance(char str[] , char ch)
+int CountOccurance(char str[], char ch)
 {
-    int iCount = 0 ;
+    int iCount = 0;
 
     while(*str != '\0')
     {
         if(*str == ch)
         {
-          iCount++;
+            iCount++;
         }
+
         str++;
     }
 
     return iCount;
-
 }
 
-int main ()
-{
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
 
+int main()
+{
     char Arr[50] = {'\0'};
-    int iRet = 0 ;
+    int iRet = 0;
     char cValue = '\0';
 
     printf("Enter string : \n");
-    scanf("%[^'\n]s",Arr);
+    scanf("%[^'\n]s", Arr);
 
-    printf("Enter the charcter :\n");
-    scanf("%c",&cValue);                    //Issue
+    printf("Enter the character : \n");
+    scanf("%c", &cValue);       // Issue
 
-    iRet = CountOccurance(Arr ,cValue);
+    iRet = CountOccurance(Arr, cValue);
 
-    printf("Number of occurance are  : %d\n",iRet);
+    printf("Number of occurance are : %d\n", iRet);
 
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : Marvellous
+//          l
+//  Output : Number of occurance are : 2
+//
+//  Input : India
+//          i
+//  Output : Number of occurance are : 1
+//
+//  Input : Hello World
+//          o
+//  Output : Number of occurance are : 2
+//
+//  Input : ABCDEF
+//          Z
+//  Output : Number of occurance are : 0
+//
+/////////////////////////////////////////////////////////////////
