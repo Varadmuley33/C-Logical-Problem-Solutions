a204d89c30bb3bb9a5c5c3d46b5b1f6f471b453f
Varad Muley <varad.muley408@gmail.com>
2026-06-19 23:37:37 +0530

    Enhance documentation in Program230.c
    Added detailed comments and author information to the Program230.c file.

--- Program230.c
+++ Program230.c
@@ -1,9 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program230.c
+//  Description :   Counts the number of capital and small characters
+//                  present in the given string.
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : CountAll
+//  Description :   Counts capital and small characters present
+//                  in the given string.
+//  Input :         Character Array
+//  Output :        Void
+//  Author :        Varad Nitin Muley
+//
+/////////////////////////////////////////////////////////////////
 
 void CountAll(char str[])
 {
-    int iCountSmall = 0 ;
+    int iCountSmall = 0;
     int iCountCapital = 0;
 
     while(*str != '\0')
@@ -16,25 +41,50 @@ void CountAll(char str[])
         {
             iCountSmall++;
         }
-        str++;
 
+        str++;
     }
 
-    printf("Count of Capital characters is : %d\n" ,iCountCapital);
-    printf("Count of Small characters is : %d\n" ,iCountSmall);
-
-
+    printf("Count of Capital characters is : %d\n", iCountCapital);
+    printf("Count of Small characters is : %d\n", iCountSmall);
 }
 
-int main ()
-{
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
 
+int main()
+{
     char Arr[50] = {'\0'};
 
     printf("Enter string : \n");
-    scanf("%[^'\n]s",Arr);
+    scanf("%[^'\n]s", Arr);
 
     CountAll(Arr);
 
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : AbcDef
+//  Output : Count of Capital characters is : 2
+//           Count of Small characters is : 4
+//
+//  Input : HELLO
+//  Output : Count of Capital characters is : 5
+//           Count of Small characters is : 0
+//
+//  Input : abcdef
+//  Output : Count of Capital characters is : 0
+//           Count of Small characters is : 6
+//
+//  Input : Marvellous
+//  Output : Count of Capital characters is : 1
+//           Count of Small characters is : 9
+//
+/////////////////////////////////////////////////////////////////
