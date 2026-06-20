3cea17c728542891f6bea273af9dbad24a8c3b3a
Varad Muley <varad.muley408@gmail.com>
2026-06-20 22:14:32 +0530

    Enhance documentation in Program220.c
    Added detailed comments and documentation for the Count function and main application.

--- Program220.c
+++ Program220.c
@@ -1,35 +1,77 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program220.c
+//  Description :   Counts the number of occurrences of characters
+//                  'a' and 'A' in the given string.
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : Count
+//  Description :   Counts the number of occurrences of characters
+//                  'a' and 'A' present in the given string.
+//  Input :         Character Array
+//  Output :        Integer
+//  Author :        Varad Nitin Muley
+//
+/////////////////////////////////////////////////////////////////
 
 int Count(char str[])
 {
-    int iCount = 0 ;
+    int iCount = 0;
 
     while(*str != '\0')
     {
         if((*str == 'A') || (*str == 'a'))
         {
-          iCount++;
+            iCount++;
         }
+
         str++;
     }
 
     return iCount;
-
 }
 
-int main ()
-{
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
 
+int main()
+{
     char Arr[50] = {'\0'};
-    int iRet = 0 ;
+    int iRet = 0;
 
     printf("Enter string : \n");
-    scanf("%[^'\n]s",Arr);
+    scanf("%[^'\n]s", Arr);
 
     iRet = Count(Arr);
 
-    printf("Count of a and A is  : %d\n",iRet);
+    printf("Count of a and A is : %d\n", iRet);
 
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : Apple
+//  Output : Count of a and A is : 1
+//
+//  Input : America
+//  Output : Count of a and A is : 2
+//
+/////////////////////////////////////////////////////////////////
