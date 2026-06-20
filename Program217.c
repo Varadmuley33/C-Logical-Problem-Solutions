560364d2db4d26f8452637f7a34fd4366e17ce6b
Varad Muley <varad.muley408@gmail.com>
2026-06-20 22:13:46 +0530

    Enhance documentation in Program217.c
    Added detailed comments and documentation for the program.

--- Program217.c
+++ Program217.c
@@ -1,9 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program217.c
+//  Description :   Calculates and returns the length of the given
+//                  string without using the library function strlen().
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : strlenX
+//  Description :   Counts the number of characters present in
+//                  the given string and returns its length.
+//  Input :         Character Array
+//  Output :        Integer
+//  Author :        Varad Nitin Muley
+//
+/////////////////////////////////////////////////////////////////
 
 int strlenX(char str[])
 {
-    int iCount = 0 ;
+    int iCount = 0;
 
     while(*str != '\0')
     {
@@ -12,21 +37,37 @@ int strlenX(char str[])
     }
 
     return iCount;
-
 }
 
-int main ()
-{
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
 
+int main()
+{
     char Arr[50] = {'\0'};
-    int iRet = 0 ;
+    int iRet = 0;
 
     printf("Enter string : \n");
-    scanf("%[^'\n]s",Arr);
+    scanf("%[^'\n]s", Arr);
 
     iRet = strlenX(Arr);
 
-    printf("Length of string is : %d\n",iRet);
+    printf("Length of string is : %d\n", iRet);
 
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : Hello
+//  Output : Length of string is : 5
+//
+//  Input : Marvellous
+//  Output : Length of string is : 10
+//
+/////////////////////////////////////////////////////////////////
