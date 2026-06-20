d6f3e5b5eb1d462731c38c69b4e3821084d328e5
Varad Muley <varad.muley408@gmail.com>
2026-06-20 22:14:03 +0530

    Refactor Program218.c with comments and author info
    Added comments and documentation for clarity.

--- Program218.c
+++ Program218.c
@@ -1,35 +1,77 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program218.c
+//  Description :   Counts the number of occurrences of character
+//                  'a' in the given string.
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : Count
+//  Description :   Counts the number of occurrences of character
+//                  'a' present in the given string.
+//  Input :         Character Array
+//  Output :        Integer
+//  Author :        Varad Nitin Muley
+//
+/////////////////////////////////////////////////////////////////
 
 int Count(char str[])
 {
-    int iCount = 0 ;
+    int iCount = 0;
 
     while(*str != '\0')
     {
         if(*str == 'a')
         {
-          iCount++;
+            iCount++;
         }
+
         str++;
     }
 
     return iCount;
-
 }
 
-int main ()
-{
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
 
+int main()
+{
     char Arr[50] = {'\0'};
-    int iRet = 0 ;
+    int iRet = 0;
 
     printf("Enter string : \n");
-    scanf("%[^'\n]s",Arr);
+    scanf("%[^'\n]s", Arr);
 
     iRet = Count(Arr);
 
-    printf("Count of a is  : %d\n",iRet);
+    printf("Count of a is : %d\n", iRet);
 
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : Marvellous
+//  Output : Count of a is : 1
+//
+//  Input : Banana
+//  Output : Count of a is : 3
+//
+/////////////////////////////////////////////////////////////////
