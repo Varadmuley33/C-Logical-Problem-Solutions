37c747378ae4c67bc99fc4b6244714f8ad1fcfb9
Varad Muley <varad.muley408@gmail.com>
2026-06-21 23:47:18 +0530

    Add comments and documentation to Program205.c
    The code now includes detailed comments explaining the purpose of the program, the required header files, and the test cases handled by the application.

--- Program205.c
+++ Program205.c
@@ -1,29 +1,73 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program205.c
+//  Description :   Accepts a file name from the user, reads
+//                  data from the file and displays it.
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 #include<fcntl.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
 int main ()
 {
-    int fd = 0 ,iRet = 0;
+    int fd = 0, iRet = 0;
     char FileName[20];
     char Data[50];
 
     printf("Enter the name of file : ");
-    scanf("%s",FileName);
+    scanf("%s", FileName);
 
-    fd = open(FileName,O_RDWR);
+    fd = open(FileName, O_RDWR);
 
-    if(fd == -1 )
+    if(fd == -1)
     {
         printf("Unable to open ");
         return -1;
     }
 
-    iRet = read(fd,Data,5);
-    printf("%d bytes gets successfully read\n",iRet);
+    iRet = read(fd, Data, 5);
+    printf("%d bytes gets successfully read\n", iRet);
 
-    printf("Data from file : %s\n",Data);
+    printf("Data from file : %s\n", Data);
 
     close(fd);
-    
+
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : Demo.txt
+//  File Content : India is my country
+//  Output : 5 bytes gets successfully read
+//           Data from file : India
+//
+//  Explanation :
+//  The first 5 bytes are read from the file and
+//  stored in the Data array. The contents read
+//  from the file are then displayed on the screen.
+//
+//  Input : Sample.txt
+//  Output : Unable to open
+//
+//  Explanation :
+//  If the specified file cannot be opened,
+//  open() returns -1 and the program terminates.
+//
+/////////////////////////////////////////////////////////////////
