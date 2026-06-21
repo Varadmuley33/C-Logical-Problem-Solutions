083a56d558b0a6b151973eff1b7d4a06196cf268
Varad Muley <varad.muley408@gmail.com>
2026-06-21 23:46:54 +0530

    Implement file writing functionality in Program203.c
    This program accepts a file name from the user and writes a predefined string into the specified file, handling errors if the file cannot be opened.

--- Program203.c
+++ Program203.c
@@ -1,27 +1,69 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program203.c
+//  Description :   Accepts a file name from the user and writes
+//                  data into the specified file.
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 #include<fcntl.h>
 
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
+
 int main ()
 {
-    int fd = 0 ,iRet = 0;
+    int fd = 0, iRet = 0;
     char FileName[20];
     char Data[] = "India is my country";
 
     printf("Enter the name of file : ");
-    scanf("%s",FileName);
+    scanf("%s", FileName);
 
-    fd = open(FileName,O_RDWR);
+    fd = open(FileName, O_RDWR);
 
-    if(fd == -1 )
+    if(fd == -1)
     {
         printf("Unable to open ");
         return -1;
     }
 
-    iRet = write(fd ,Data,11);
-    printf("%d bytes gets successfully return\n",iRet);
+    iRet = write(fd, Data, 11);
+    printf("%d bytes gets successfully return\n", iRet);
 
     close(fd);
-    
+
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : Demo.txt
+//  Output : 11 bytes gets successfully return
+//
+//  Explanation :
+//  The file is opened successfully and the first
+//  11 bytes from the string "India is my country"
+//  are written into the file.
+//
+//  Input : Sample.txt
+//  Output : Unable to open
+//
+//  Explanation :
+//  If the specified file cannot be opened,
+//  open() returns -1 and the program terminates.
+//
+/////////////////////////////////////////////////////////////////
