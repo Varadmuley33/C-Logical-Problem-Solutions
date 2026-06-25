63046135e16c6e078e381feb5924b6702fe1a286
Varad Muley <varad.muley408@gmail.com>
2026-06-25 23:09:36 +0530

    Update program71.c

--- program71.c
+++ program71.c
@@ -1,5 +1,29 @@
-// Addition of digits 
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program71.c
+//  Description :   Accept a number from the user and return the sum of
+//                  its digits.
+//  Author :        Varad Nitin Muley
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : SumDigits
+//  Description   : Calculates and returns the sum of digits of the given
+//                  number.
+//  Input         : Integer
+//  Output        : Integer
+//
+/////////////////////////////////////////////////////////////////
 
 int SumDigits(int iNo)
 {
@@ -15,25 +39,29 @@ int SumDigits(int iNo)
      {
           return 1;
      }
+
      while(iNo != 0)
      {
           iDigit = iNo % 10;
           iNo = iNo / 10;
 
           iSum = iSum + iDigit ;
-
      }
 
      return iSum;
-
-
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+/////////////////////////////////////////////////////////////////
 
 int main ()
 {
      int iValue = 0;
      int iRet = 0 ;
-     
+
      printf("Enter the number : \n");
      scanf("%d",&iValue);
 
@@ -41,5 +69,18 @@ int main ()
      printf("Sum of digits : %d\n",iRet);
 
      return 0;
-
 }
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//  Input : 1234
+//  Output :
+//  Sum of digits : 10
+//
+//  Input : -567
+//  Output :
+//  Sum of digits : 18
+//
+/////////////////////////////////////////////////////////////////
