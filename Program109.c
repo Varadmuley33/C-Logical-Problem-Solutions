b5dd96a5eaaf9482a1dccf9f3dcf6bd84832b166
Varad Muley <varad.muley408@gmail.com>
2026-08-10 23:33:05 +0530

    Format Program109.c for consistency

--- Program109.c
+++ Program109.c
@@ -3,7 +3,7 @@ int main ()
 
     int Arr[] = {10,20,30,40};
 
-    printf("%d\n", Arr);
+    printf("%d\n", Arr);   
     printf("%d\n", &Arr);
     printf("%d\n", &Arr[0]);
     printf("%lu\n",sizeof(Arr));
