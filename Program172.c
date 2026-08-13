0aced1aa27e499e1786e408928c49cdd489e83a5
Varad Muley <varad.muley408@gmail.com>
2026-08-13 23:44:21 +0530

    Remove redundant comments and clean up code

--- Program172.c
+++ Program172.c
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //  File name :     program172.c
-//  Description :   
+//  Description :     
 //  Author :        Varad Nitin Muley
 //  Date :          18/11/2025
 //  Input :         5
