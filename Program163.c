c276d6969395428a5b3b1c4ca2ca44ef428b8488
Varad Muley <varad.muley408@gmail.com>
2026-08-22 23:21:34 +0530

    Remove redundant comments and clean up code

--- Program163.c
+++ Program163.c
@@ -3,7 +3,7 @@
 //  File name :     program163.c
 //  Description :      
 //  Author :        Varad Nitin Muley
-//  Date :          18/11/2025
+//  Date :          18/11/2025   
 //  Input :         6
 //  Output :        1 * 2 * 3 *  
 //                  1 2 3 4 5 6 
