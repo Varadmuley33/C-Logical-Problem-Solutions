fd8f015cd1d646a23be9e805456507e49fc3ea6f
Varad Muley <varad.muley408@gmail.com>
2026-08-26 23:36:03 +0530

    Remove commented-out code and clean up formatting

--- Program190.c
+++ Program190.c
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //  File name :     program190.c
-//  Description :   
+//  Description :      
 //  Author :        Varad Nitin Muley
 //  Date :          18/11/2025   
 //  Input :         6
