b7f07ace07ba237130a16d5dd086a8750ee3ad76
Varad Muley <varad.muley408@gmail.com>
2026-09-07 23:34:31 +0530

    Refactor CountDigits.c with consistent formatting

--- Program232.c
+++ Program232.c
@@ -2,7 +2,7 @@
 //
 //  File name :     CountDigits.c
 //  Description :   Counts the number of digits present in the
-//                  given string.
+//                  given string.  
 //  Author :        Varad Nitin Muley
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
