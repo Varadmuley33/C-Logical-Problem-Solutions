28a2fb61d8cf532d36b818ed5f5ed7d435bfc4fd
Varad Muley <varad.muley408@gmail.com>
2026-09-08 23:02:25 +0530

    Remove redundant comments and format code

--- Program230.c
+++ Program230.c
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //  File name :     Program230.c
-//  Description :   Counts the number of capital and small characters
+//  Description :   Counts the number of capital and small characters    
 //                  present in the given string.
 //  Author :        Varad Nitin Muley
 //
