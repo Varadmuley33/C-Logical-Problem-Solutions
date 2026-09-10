9f4de78af63c930d5a0215f5d4075a46252c95ce
Varad Muley <varad.muley408@gmail.com>
2026-09-10 23:44:16 +0530

    Reformat comments and structure in Program271.c

--- Program271.c
+++ Program271.c
@@ -3,7 +3,7 @@
 //  File name :     Program271.c
 //  Description :   Calculates and displays the summation of all
 //                  elements from a float array.
-//  Author :        Varad Nitin Muley
+//  Author :        Varad Nitin Muley   
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
